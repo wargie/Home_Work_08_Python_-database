5ffec8e537a275261bc156278cbeb390303fb494
wargie <wargie@bk.ru>
2022-05-24 11:45:58 +0200

    исправленоа ошибка пропуска

--- controller.py
+++ controller.py
@@ -1,8 +1,8 @@
 from sort import sort,get_da
-from viev import get_number_operasion,get_data
+from viev import get_number_operation,get_data
 from write_read_file import read_file,write_file
 from main import sort_data
 
 
 def button():
-    return sort_data(get_number_operasion,read_file,write_file,sort,get_da,get_data)
+    return sort_data(get_number_operation,read_file,write_file,sort,get_da,get_data)
